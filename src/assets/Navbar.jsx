01312242ca1693cf407164d0c4b2bd4ac609f3be
Amit Kumar Mishra <amitmishraa651@gmail.com>
2024-10-16 11:12:29 +0530

    first commit

--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -3,40 +3,38 @@ import './Navbar.css'
 import Light from './day.png'
 import Dark from './night.png'
 
-const Navbar = ({theme,settheme}) => {
-    const toggle_mode =()=>{
-        theme =='light'? settheme('dark'): settheme('light');
-    }
+ Navbar({ theme, settheme }) {
+  const toggle_mode = () => {
+    theme == 'light' ? settheme('dark') : settheme('light');
+  };
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [dates, setDates] = useState(new Date().toLocaleDateString());
 
-
-    const [time, setTime]=useState(new Date().toLocaleTimeString());
-    const [dates,setDates]=useState(new Date().toLocaleDateString());
-
-    useEffect(()=>{
-      const tii= setInterval(()=>{
-        setTime(new Date().toLocaleTimeString())
-        setDates(new Date().toLocaleDateString())
-      }, 1000);
-      return ()=> clearInterval(tii);
-    }, [])
+  useEffect(() => {
+    const tii = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+      setDates(new Date().toLocaleDateString());
+    }, 1000);
+    return () => clearInterval(tii);
+  }, []);
   return (
     <div className="navbar">
-      
-        <div className="title">
-            <h1>Today
-            <span style={{color: "#4CAF50"}}>Jocks</span></h1>
-        </div>
-        <div className="time">
-            <h3 id="current-time" >Time : {time}<br/>   Date  :  {dates}</h3>
-        </div>
 
-        <div className='img-them'>
-        <img onClick={()=>{toggle_mode()}} src={theme == 'light' ? Dark : Light} alt="" />
-        
-        </div>
-      
+      <div className="title">
+        <h1>Today
+          <span style={{ color: "#4CAF50" }}>Jocks</span></h1>
+      </div>
+      <div className="time">
+        <h3 id="current-time">Time : {time}<br />   Date  :  {dates}</h3>
+      </div>
+
+      <div className='img-them'>
+        <img onClick={() => { toggle_mode(); } } src={theme == 'light' ? Dark : Light} alt="" />
+
+      </div>
+
     </div>
-  )
+  );
 }
 
 export default Navbar
