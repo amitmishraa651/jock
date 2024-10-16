9b04c41d181db60f4b9217b2932ee1204750d1e7
Amit Kumar Mishra <amitmishraa651@gmail.com>
2024-10-16 10:44:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import './App.css';
+import Navbar from './assets/Navbar';
+
+const App = () => {
+  const [theme,settheme]=useState('light');
+  const[jock,setjock]=useState({});
+  useEffect(()=>{
+    fetch('https://official-joke-api.appspot.com/random_joke')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setjock(data);
+    })
+    .catch(err => console.log(err));
+  }, [])
+  return (
+    <div className={`container ${theme}`} >
+      <Navbar theme={theme} settheme={settheme} />
+      <div className='jock-main'>
+        <div className='jock'>
+          <h1>Today Jocks</h1>
+          <p>{jock.setup}</p>
+          <p>{jock.punchline}</p>
+          <button onClick={()=> window.location.reload(false)}>Next</button>
+        </div>
+      </div>
+      
+
+      <div className='footer'> 
+        <p> Copyright &#169; 2024, Created by Amit.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import './Navbar.css'
+import Light from './day.png'
+import Dark from './night.png'
+
+const Navbar = ({theme,settheme}) => {
+    const toggle_mode =()=>{
+        theme =='light'? settheme('dark'): settheme('light');
+    }
+
+
+    const [time, setTime]=useState(new Date().toLocaleTimeString());
+    const [dates,setDates]=useState(new Date().toLocaleDateString());
+
+    useEffect(()=>{
+      const tii= setInterval(()=>{
+        setTime(new Date().toLocaleTimeString())
+        setDates(new Date().toLocaleDateString())
+      }, 1000);
+      return ()=> clearInterval(tii);
+    }, [])
+  return (
+    <div className="navbar">
+      
+        <div className="title">
+            <h1>Today
+            <span style={{color: "#4CAF50"}}>Jocks</span></h1>
+        </div>
+        <div className="time">
+            <h3 id="current-time" >Time : {time}<br/>   Date  :  {dates}</h3>
+        </div>
+
+        <div className='img-them'>
+        <img onClick={()=>{toggle_mode()}} src={theme == 'light' ? Dark : Light} alt="" />
+        
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
